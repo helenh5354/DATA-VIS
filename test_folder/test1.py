922d1808259e59ec49c12553d13c99a5409e9bd9
Helen Harod <helenharrod53@gmail.com>
2025-11-12 18:08:09 +0000

    Second commit

--- test_folder/test1.py
+++ test_folder/test1.py
@@ -1,5 +1,5 @@
 # This script takes two numbers as input and prints their sum
-def add_two_number(num1,num2):
+def add_two_number(num1, num2):
     return num1 + num2
 
 
@@ -7,4 +7,4 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 result = add_two_number(num1, num2)
-print("The sum is:", result)    
+print("The sum is:", result)
